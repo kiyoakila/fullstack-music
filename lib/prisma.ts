7aeba57ddef878e2aaa0d8b63718f20e67821790
Ruiqi Huang <kiyoakila@gmail.com>
2022-07-04 23:27:46 -0700

    update schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-export default new PrismaClient()
+/** @type {PrismaClient} */
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+prisma.$connect()
+export default prisma
